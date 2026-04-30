7d8a0a847417bda1178444a03d81fbdecff3651a
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-04-30 15:35:17 +0000

    Remove unused import

--- src/main/java/com/github/warriorjacq9/terraindiffusionbp/world/TerrainDiffusionChunkGenerator.java
+++ src/main/java/com/github/warriorjacq9/terraindiffusionbp/world/TerrainDiffusionChunkGenerator.java
@@ -21,8 +21,6 @@ import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.StructuresConfig;
 import net.minecraft.world.gen.chunk.VerticalBlockSample;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.Optional;
